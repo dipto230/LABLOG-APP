f282995ffc1dd84714f83a113de5f46564aebe59
dipto230 <diptoroy230305@gmail.com>
2026-01-25 21:23:47 +0530

    everything done without better-auth

--- src/modules/usage-log/log.controller.ts
+++ src/modules/usage-log/log.controller.ts
@@ -1,11 +1,12 @@
 import { RequestHandler } from "express";
 import { prisma } from "../../lib/prisma";
 
-const createUsagesLog:RequestHandler = async (req, res) => {
+const createUsagesLog: RequestHandler = async (req, res) => {
+    console.log(req.user)
     try {
         const payload = req.body;
         const log = await prisma.usageLog.create({
-            data:payload
+            data:{...payload, userId: req.user.id}
         })
         res.send({message:"log added", data:log})
     } catch (error) {
@@ -29,9 +30,30 @@ const getUsagesLog:RequestHandler = async (req, res) => {
     }
 }
 
+const updateUsageLog: RequestHandler = async (req, res) => {
+    const { id } = req.params;
+    if(!id) return res.send("Please Provide an id")
+    
+    try {
+        const log = await prisma.usageLog.update({
+            where: { id },
+            data: {
+                endTime: new Date()
+            }
+        });
+        res.send({message:"log", data:log})
+        
+    } catch (error) {
+        res.send({ message: "log getting error", error })
+        
+        
+    }
+}
+
 export const logController = {
     createUsagesLog,
-    getUsagesLog
+    getUsagesLog,
+    updateUsageLog
 }
 
 
--- src/modules/usage-log/log.router.ts
+++ src/modules/usage-log/log.router.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { logController } from "./log.controller";
+import auth from "../../middleware/auth";
+import { Role } from "../../generated/prisma/enums";
 
 
 
 const logRouter = Router()
 
-logRouter.post("/", logController.createUsagesLog)
+logRouter.post("/", auth(), logController.createUsagesLog)
 logRouter.get("/", logController.getUsagesLog)
+logRouter.patch("/:id", auth(), logController.updateUsageLog)
 
 
 export default  logRouter
